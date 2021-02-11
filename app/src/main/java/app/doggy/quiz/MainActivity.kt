50223dcb357e09e8183c5ff51a3e379288dc7ea5
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-11 13:38:16 +0900

    起動画面完成

---
+++ app/src/main/java/app/doggy/quiz/MainActivity.kt
@@ -0,0 +1,22 @@
+package app.doggy.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Intentをインスタンス化
+        val quizIntent = Intent(applicationContext, QuizActivity::class.java)
+
+        //クリックリスナを設定。
+        startButton.setOnClickListener{
+            //画面遷移。
+            startActivity(quizIntent)
+        }
+    }
+}
